aca35580c4a5d426b501be6cbdaa8b7ef4cf8fca
Ulich, Timo <timo@ulich.net>
2018-09-01 23:40:56 +0200

    describe ports better

--- device/device.ino
+++ device/device.ino
@@ -6,6 +6,9 @@
 
 #define LED_3 11
 
+#define TRANSMITTING_LED LED_3
+#define THERMISTOR_PORT A0
+
 static osjob_t sendjob;
 const unsigned TX_INTERVAL = 30;
 
@@ -16,7 +19,7 @@ void onEvent (ev_t ev) {
         case EV_TXCOMPLETE:
             Serial.println(" ");
             Serial.println("TX COMPLETE");
-            digitalWrite(LED_3, LOW);
+            digitalWrite(TRANSMITTING_LED, LOW);
             os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
             break;
         default:
@@ -29,14 +32,14 @@ void do_send(osjob_t* job) {
         return;
     }
 
-    digitalWrite(LED_3, HIGH);
+    digitalWrite(TRANSMITTING_LED, HIGH);
 
     payload_type types[LORA_FIELD_COUNT];
     lora_payload fields[LORA_FIELD_COUNT];
     lora_prepareTypes(types);
 
     types[0] = PAYLOAD_TYPE_FLOAT;
-    fields[0].f = thermistor_measureTemperature(A0);
+    fields[0].f = thermistor_measureTemperature(THERMISTOR_PORT);
 
     lora_send(types, fields);
 }
@@ -47,10 +50,10 @@ void do_send(osjob_t* job) {
 void setup() {
     Serial.println("SCS Edu-Kit init");
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(LED_3, OUTPUT);
-
+    pinMode(TRANSMITTING_LED, OUTPUT);
+    
     digitalWrite(LED_BUILTIN, HIGH);
-    digitalWrite(LED_3, LOW);
+    digitalWrite(TRANSMITTING_LED, LOW);
 
     os_init();
     
