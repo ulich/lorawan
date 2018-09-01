12d768c341d7b6b8edc438f29c5cd4b78170299d
Ulich, Timo <timo@ulich.net>
2018-09-01 21:37:09 +0200

    clean up LEDs

--- iot.ino
+++ iot.ino
@@ -1,14 +1,10 @@
-#define LED_4 10 // LED 4
-#define LED_3 9 // 16  LED 3
-#define LED_2 6 // 15  LED 2
-#define LED_1 5 // LED 1
-
-
 #include <stdlib.h>
 #include <lmic.h>
 #include "utils.h"
 #include "lora.h"
 #include "thermistor.h"
+
+#define LED_3 11
 
 static osjob_t sendjob;
 const unsigned TX_INTERVAL = 30;
@@ -21,8 +17,6 @@ void onEvent (ev_t ev) {
             Serial.println(" ");
             Serial.println("TX COMPLETE");
             digitalWrite(LED_3, LOW);
-            digitalWrite(LED_1, LOW);
-            digitalWrite(LED_BUILTIN, LOW);
             os_setTimedCallback(&sendjob, os_getTime() + sec2osticks(TX_INTERVAL), do_send);
             break;
         default:
@@ -41,14 +35,10 @@ void do_send(osjob_t* job) {
     lora_payload fields[LORA_FIELD_COUNT];
     lora_prepareTypes(types);
 
-    
     types[0] = PAYLOAD_TYPE_FLOAT;
     fields[0].f = thermistor_measureTemperature(A0);
 
-
     lora_send(types, fields);
-
-    digitalWrite(LED_BUILTIN, HIGH);
 }
 
 
@@ -57,11 +47,11 @@ void do_send(osjob_t* job) {
 void setup() {
     Serial.println("SCS Edu-Kit init");
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(LED_4, OUTPUT);
-    digitalWrite(LED_4, HIGH);  // power on
     pinMode(LED_3, OUTPUT);
-    pinMode(LED_2, OUTPUT);
-    pinMode(LED_1, OUTPUT);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_3, LOW);
+
     os_init();
     
     Serial.begin(9600);
